87aa91ea71abdb79f0c8ce023e435f77a55f1be4
arnold2111 <43273915+arnold2111@users.noreply.github.com>
2018-10-23 09:51:04 +0200

    Update Billiards.cpp

--- Billiards/src/Billiards.cpp
+++ Billiards/src/Billiards.cpp
@@ -1,6 +1,6 @@
 //============================================================================
 // Name        : Billiards.cpp
-// Author      :
+// Author      : Eduardo Guti
 // Version     :
 // Copyright   : Your copyright notice
 // Description : Hello World in C++, Ansi-style
